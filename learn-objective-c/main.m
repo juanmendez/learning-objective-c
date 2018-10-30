eba4b959c25c04a9aaaa05785bfae05d59609603
Juan Mendez <juan.mendez@grainger.com>
2018-10-30 18:01:13 -0500

    first comments

--- learn-objective-c/main.m
+++ learn-objective-c/main.m
@@ -8,10 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+//like C++, every project requires main
 int main(int argc, const char * argv[]) {
+    //memory management..
     @autoreleasepool {
-        // insert code here...
+        //distant relative of Log.i(TAG, "Hello World);
         NSLog(@"Hello, World!");
     }
+    
+    //marks successful conclusion
     return 0;
 }
