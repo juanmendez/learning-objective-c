ff16815d2139342249837761f756f5ade9330f67
Juan Mendez <juan.mendez@grainger.com>
2018-10-30 17:44:45 -0500

    Initial Commit

---
+++ learn-objective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  learn-objective-c
+//
+//  Created by Mendez, Juan on 10/30/18.
+//  Copyright © 2018 juanmendez.info. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
